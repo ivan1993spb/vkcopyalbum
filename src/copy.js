f870319f815c7258c0328e936f817ff963500d84
Ivan Pushkin <pushkin13@bk.ru>
2016-07-25 02:27:14 +0300

    created photo sizes

--- src/copy.js
+++ src/copy.js
@@ -6,20 +6,20 @@ var VK = require('vkapi');
 var cmd_blocks = require('./cmd_blocks.js');
 // var album_list = require('./album_list.js');
 
+// Copy album
 exports.copy = function (album) {
-	console.log("ok", album);
-
 	VK.api("photos.get", {
-		owner_id: album.owner_id,
-		album_id: album.id
+		owner_id:    album.owner_id,
+		album_id:    album.id,
+		photo_sizes: 1
 	}, function(data) {
-		console.log(data)
-		
 		if (data.error) {
             cmd_blocks.showMessage("cannot load photo: "+data.error);
 			console.log("cannot load photo: ", data.error);
 		} else if (data.response.count > 0) {
-			// console.log(data)
+			data.response.items.forEach(function(photo){
+				console.log("photo", photo.sizes[photo.sizes.length-1]);
+			});
 		}
 	})
 };
